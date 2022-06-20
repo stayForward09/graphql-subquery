8811014b34bf2167552fab7386bfc923877a4c67
AnichoKraus <anichokraus3123@gmail.com>
2022-06-20 05:24:51 -0700

    only vesting schedule

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 //Exports all handler functions
-export * from './mappings/mappingHandlers'
-export * from './mappings/mappingSystemTokenHandlers'
+// export * from './mappings/mappingHandlers'
+// export * from './mappings/mappingSystemTokenHandlers'
 export * from './mappings/mappingVestingScheduleHandler'
