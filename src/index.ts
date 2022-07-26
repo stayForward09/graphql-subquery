fc42ca312107c9b40d0a54e00b03f3e91e29b473
AnichoKraus <anichokraus3123@gmail.com>
2022-07-26 06:37:33 -0700

    add failed vesting schedule

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from "./mappings/mappingHandlers";
 export * from './mappings/mappingSystemTokenHandlers'
 export * from './mappings/mappingVestingScheduleHandler'
 export * from './mappings/mappingAllocationHandler'
+export * from './mappings/mappingVestingScheduleFailedHandler'
 import "@polkadot/api-augment";
