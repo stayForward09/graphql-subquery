8f712742df90b4a8b602a7e0e818cb92eaab3a6d
AnichoKraus <anichokraus3123@gmail.com>
2022-12-22 16:44:53 -0400

    delete other related index data and files

--- src/helpers/utils.ts
+++
@@ -1,8 +0,0 @@
-import { Codec } from '@polkadot/types/types';
-const kickedAccounts = new Set<string>();
-
-kickedAccounts.add('4jbtsgNhpGAzdEGrKRb7g8Mq4ToNUpBVxeye942tWfG3gcYi');
-
-export const isKicked = (account: Codec): boolean => {
-    return kickedAccounts.has(account.toString());
-};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import '@polkadot/types-augment';
 import "@polkadot/api-augment";
 
-export * from './mappings/mappingVestingHandlers'
 export * from './mappings/mappingAllocationHandler'
-export * from './mappings/mappingBalanceHandlers'
 export * from './mappings/mappingUniquesHandlers'
-export * from './mappings/mappingSystemHandler'
+export * from './mappings/mappingBalancesHandlers'
